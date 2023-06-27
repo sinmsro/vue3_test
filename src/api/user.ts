15ce44ca6c9ab161b6ecf65e7ac1a8a2e1e91f63
youki <sinmsro@163.com>
2023-06-27 08:27:28 +0800

    modify root url

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const login = () => {
     // 返回的数据格式可以和服务端约定
-    return axios.get('login');
+    return axios.post('/api/login');
 }
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [],
   configureWebpack: {
     devServer: {
       proxy: {
-        'api': {
+        '/api': {
           target: "https://www.fastmock.site/mock/ca2127d863151290af0657a8b7084836/api",
           changeOrigin: true,
+          secure:true, //接受对方是https的接口,
+          pathRewrite: {'^/api' : ''}
+
         }
       }
     }
